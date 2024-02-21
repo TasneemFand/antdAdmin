d5035e3cc0a831a31b311e1e1e9e151fc9d5d07a
Tasneem Fandakli <tasnimtepe@gmail.com>
2024-02-21 08:53:31 +0300

    initial commit: create vite app and install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+        This is a test page for the React app. It should display "Hello, World!"
+        in an h1 element.
+      </div>
+    </>
+  );
+}
+
+export default App;
